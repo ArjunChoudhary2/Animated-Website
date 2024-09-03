b6b3ff8d23762355ef74baa767020da9a1c8c39a
ArjunChoudhary2 <105961121+ArjunChoudhary2@users.noreply.github.com>
2024-09-03 09:39:12 +0530

    Cursor Implemented, cursor scroll issue fixed

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-let cursor = document.querySelector("#cursor");
-let cursorBlur = document.querySelector('#cursor-blur')
+var crsr = document.querySelector("#cursor");
+var blurcr = document.querySelector("#cursor-blur");
 
 document.addEventListener("mousemove", function (dets) {
-  cursor.style.left = dets.x  + "px";
-  cursor.style.top = dets.y + "px";
-  cursorBlur.style.left = dets.x  + "px";
-  cursorBlur.style.top = dets.y  +  "px";
+  crsr.style.left = dets.x -10+ "px";
+  crsr.style.top = dets.y -10 + "px";
+  blurcr.style.left = dets.x -200  + "px";
+  blurcr.style.top = dets.y -200  + "px";
 });
 
 gsap.to("#nav", {
@@ -15,6 +15,7 @@ gsap.to("#nav", {
   scrollTrigger: {
     trigger: "#nav",
     scroller: "body",
+    // markers:true,
     start: "top -10%",
     end: "top -11%",
     scrub: 1,
@@ -26,7 +27,8 @@ gsap.to("#main", {
   scrollTrigger: {
     trigger: "#main",
     scroller: "body",
-    start: "top -30%",
+    // markers: true,
+    start: "top -25%",
     end: "top -70%",
     scrub: 2,
   },
